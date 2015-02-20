86dd1d36b9be8e570335d30461219a54b1aba67c
cwmonkey <wackoburns@hotmail.com>
2015-02-19 21:19:47 -0800

    Updating css for menu/buttons in left column. Updating room icons. Adding ads. Refactoring staff code to reduce duplicate code.

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ var game_items = {
 		type: 'furniture',
 		description: 'Doubles prestige of items above/below (does not stack)',
 		cost: 50,
-		dirty_chance: 0.01,
+		dirty_chance: 0.005,
 		prestige: 1,
 		boost_prestige_up: 2,
 		boost_prestige_down: 2
@@ -64,7 +64,7 @@ var game_items = {
 		type: 'furniture',
 		description: 'Doubles prestige of items left/right (does not stack)',
 		cost: 50,
-		dirty_chance: 0.01,
+		dirty_chance: 0.005,
 		prestige: 1,
 		boost_prestige_left: 2,
 		boost_prestige_right: 2
@@ -116,6 +116,7 @@ var game_items = {
 
 (function() {
 
+var app = {};
 var ticks_per_second = 2;
 var rows = 20;
 var columns = 20;
@@ -126,6 +127,7 @@ var now_ms;
 var ticks;
 var i;
 var j;
+var e;
 var casino_items = [];
 var $casino = document.getElementById('casino');
 var $items = document.getElementById('items');
@@ -134,25 +136,17 @@ var $power = document.getElementById('power');
 var $power_total = document.getElementById('power_total');
 var $vault = document.getElementById('vault');
 var $prestige = document.getElementById('prestige');
-var $janitors_max = document.getElementById('janitors_max');
-var $janitors = document.getElementById('janitors');
-var $janitor_add = document.getElementById('janitor_add');
-var $janitor_remove = document.getElementById('janitor_remove');
-var $engineers_max = document.getElementById('engineers_max');
-var $engineers = document.getElementById('engineers');
-var $engineer_add = document.getElementById('engineer_add');
-var $engineer_remove = document.getElementById('engineer_remove');
-var $waitresses = document.getElementById('waitresses');
-var $waitresses_max = document.getElementById('waitresses_max');
-var $waitress_add = document.getElementById('waitress_add');
-var $waitress_remove = document.getElementById('waitress_remove');
 var $patrons = document.getElementById('patrons');
+var $pause = document.getElementById('pause');
+var $main = document.getElementById('main');
 var janitoring_reg = / \( state_janitoring \)/;
 var janitoring_class = ' ( state_janitoring )';
 var engineering_reg = / \( state_engineering \)/;
 var engineering_class = ' ( state_engineering )';
 var selected_reg = / \( selected \)/;
 var selected_class = ' ( selected )';
+var paused_reg = / \( paused \)/;
+var paused_class = ' ( paused )';
 var broken_reg = / \( state_broken \)/;
 var broken_class = ' ( state_broken )';
 var dirty_reg = / \( state_dirty \)/;
@@ -166,13 +160,11 @@ var janitors_available;
 var engineers_available;
 var fix_required = 5;
 var cleaning_required = 5;
+var paused = true;
 
 var no_power_threshold = 5;
 var no_pay_threshold = 5;
 var quit_threshold = 8;
-var janitor_quit_reasons = 0;
-var engineer_quit_reasons = 0;
-var waitress_quit_reasons = 0;
 
 var patrons = 0;
 var patrons_available;
@@ -186,25 +178,85 @@ var money = 1000000;
 var power = 0;
 var prestige = 0;
 var vault = base_vault;
+var rent_per_square = 5;
 
-var janitors_max = 0;
-var janitors = 0;
-var janitor_cost = 50;
-var engineers_max = 0;
-var engineers = 0;
-var engineer_cost = 180;
-var waitresses_max = 0;
-var waitresses = 0;
-var waitress_cost = 90;
+// TODO: Put the "static" stuff elsewhere
+var staff = [
+	{
+		singular: 'janitor',
+		plural: 'janitors',
+		cost: 50,
+		room_type: 'janitor_closet',
+		$count: null,
+		$max: null,
+		quit_reasons: 0,
+		max: 0,
+		count: 0
+	},
+	{
+		singular: 'engineer',
+		plural: 'engineers',
+		cost: 180,
+		room_type: 'engineer_room',
+		$count: null,
+		$max: null,
+		quit_reasons: 0,
+		max: 0,
+		count: 0
+	},
+	{
+		singular: 'waitress',
+		plural: 'waitresses',
+		cost: 90,
+		room_type: 'changing_room',
+		$count: null,
+		$max: null,
+		quit_reasons: 0,
+		max: 0,
+		count: 0
+	}
+];
+
+var ads = [
+	{
+		name: 'tv',
+		cost: 100,
+		count: 0,
+		prestige: 100,
+		titleTpl: null,
+		$count: null,
+		$title_element: null
+	},
+	{
+		name: 'radio',
+		cost: 10,
+		count: 0,
+		prestige: 10,
+		titleTpl: null,
+		$count: null,
+		$title_element: null
+	},
+	{
+		name: 'billboard',
+		cost: 5,
+		count: 0,
+		prestige: 5,
+		titleTpl: null,
+		$count: null,
+		$title_element: null
+	}
+];
 
 // Update variables, I guess
 var update_info = function() {
 	power = 0;
 	prestige = 0;
 	vault = base_vault;
-	janitors_max = 0;
-	engineers_max = 0;
-	waitresses_max = 0;
+
+	for ( e = 0; e < staff.length; e++ ) {
+		var employee = staff[e];
+		employee.max = 0;
+	}
 
 	for ( i = 0; i < rows; i++ ) {
 		for ( j = 0; j < columns; j++ ) {
@@ -230,16 +282,11 @@ var update_info = function() {
 						vault += item.vault;
 					}
 
-					if ( item.janitors ) {
-						janitors_max += item.janitors;
-					}
-
-					if ( item.engineers ) {
-						engineers_max += item.engineers;
-					}
-
-					if ( item.waitresses ) {
-						waitresses_max += item.waitresses;
+					for ( e = 0; e < staff.length; e++ ) {
+						var employee = staff[e];
+						if ( item[employee.plural] ) {
+							employee.max += item[employee.plural];
+						}
 					}
 
 					if ( item.prestige ) {
@@ -294,30 +341,38 @@ var update_info = function() {
 		}
 	}
 
-	if ( janitors > janitors_max ) {
-		janitors = janitors_max;
+	for ( e = 0; e < staff.length; e++ ) {
+		var employee = staff[e];
+
+		if ( employee.count > employee.max ) {
+			employee.count = employee.max;
+		}
+
+		employee.$max.innerHTML = employee.max;
+		employee.$count.innerHTML = employee.count;
+
+		if ( employee.singular == 'waitress' ) {
+			prestige += employee.count * waitress_prestige;
+		}
 	}
 
-	if ( engineers > engineers_max ) {
-		engineers = engineers_max;
-	}
+	for ( e = 0; e < ads.length; e++ ) {
+		var ad = ads[e];
 
-	if ( waitresses > waitresses_max ) {
-		waitresses = waitresses_max;
-	}
+		for ( var f = 1; f <= ad.count; f++ ) {
+			if ( money > vault * -1 ) {
+				prestige += ad.prestige;
+				money -= ad.cost;
+			}
+		}
 
-	prestige += waitresses * waitress_prestige;
+		ad.$count.innerHTML = ad.count;
+	}
 
 	$prestige.innerHTML = prestige;
 	$power.innerHTML = power;
 	$power_total.innerHTML = power;
 	$vault.innerHTML = vault;
-	$janitors_max.innerHTML = janitors_max;
-	$janitors.innerHTML = janitors;
-	$engineers_max.innerHTML = engineers_max;
-	$engineers.innerHTML = engineers;
-	$waitresses_max.innerHTML = waitresses_max;
-	$waitresses.innerHTML = waitresses;
 	info_updated = true;
 }
 
@@ -330,34 +385,23 @@ var do_item = function(settings, up, down, left, right) {
 	var item = game_items[settings.item_id];
 	var patron_count = '';
 
-	if ( item.type == 'janitor_closet' ) {
-		if ( item.power_usage > power && janitors ) {
-			janitor_quit_reasons++;
-			if ( janitor_quit_reasons == quit_threshold ) {
-				janitor_quit_reasons = 0;
-				janitors--;
-				$janitors.innerHTML = janitors;
+	for ( e = 0; e < staff.length; e++ ) {
+		var employee = staff[e];
+
+		if ( item.type == employee.room_type ) {
+			if ( item.power_usage > power && employee.count ) {
+				employee.quit_reasons++;
+
+				if ( employee.quit_reasons == quit_threshold ) {
+					employee.quit_reasons = 0;
+					employee.count--;
+					employer.$count.innerHTML = employee.count;
+				}
 			}
 		}
-	} else if ( item.type == 'engineer_room' ) {
-		if ( item.power_usage > power && engineers ) {
-			engineer_quit_reasons++;
-			if ( engineer_quit_reasons == quit_threshold ) {
-				engineer_quit_reasons = 0;
-				engineers--;
-				$engineers.innerHTML = engineers;
-			}
-		}
-	} else if ( item.type == 'changing_room' ) {
-		if ( item.power_usage > power && waitresses ) {
-			waitress_quit_reasons++;
-			if ( waitress_quit_reasons == quit_threshold ) {
-				waitress_quit_reasons = 0;
-				waitresses--;
-				$waitresses.innerHTML = waitresses;
-			}
-		}
-	} else if ( item.break_chance && settings.durability && ( !item.power_usage || item.power_usage <= power ) ) {
+	}
+
+	if ( item.break_chance && settings.durability && ( !item.power_usage || item.power_usage <= power ) ) {
 		if ( item.power_usage ) {
 			power -= item.power_usage;
 			$power.innerHTML = power;
@@ -471,6 +515,7 @@ var real_tick = function() {
 			var down = ( i < rows - 1 ) ? casino_items[i + 1][j] : null;
 			var left = ( j ) ? casino_items[i][j - 1] : null;
 			var right = ( j < columns - 1 ) ? casino_items[i][j + 1] : null;
+			money -= rent_per_square;
 
 			if ( settings ) {
 				do_item(settings, up, down, left, right);
@@ -478,44 +523,20 @@ var real_tick = function() {
 		}
 	}
 
-	for ( i = 0; i < janitors; i++ ) {
-		if ( money > vault * -1 ) {
-			money -= janitor_cost;
-		} else {
-			janitor_quit_reasons++;
+	for ( e = 0; e < staff.length; e++ ) {
+		var employee = staff[e];
 
-			if ( janitor_quit_reasons == quit_threshold ) {
-				janitor_quit_reasons = 0;
-				janitors--;
-				$janitors.innerHTML = janitors;
-			}
-		}
-	}
+		for ( i = 0; i < janitors; i++ ) {
+			if ( money > vault * -1 ) {
+				money -= employee.cost;
+			} else {
+				employee.quit_reasons++;
 
-	for ( i = 0; i < engineers; i++ ) {
-		if ( money > vault * -1 ) {
-			money -= engineer_cost;
-		} else {
-			engineer_quit_reasons++;
-
-			if ( engineer_quit_reasons == quit_threshold ) {
-				engineer_quit_reasons = 0;
-				engineers--;
-				$engineers.innerHTML = engineers;
-			}
-		}
-	}
-
-	for ( i = 0; i < waitresses; i++ ) {
-		if ( money > vault * -1 ) {
-			money -= waitress_cost;
-		} else {
-			waitress_quit_reasons++;
-
-			if ( waitress_quit_reasons == quit_threshold ) {
-				waitress_quit_reasons = 0;
-				waitresses--;
-				$waitresses.innerHTML = waitresses;
+				if ( employee.quit_reasons == quit_threshold ) {
+					employee.quit_reasons = 0;
+					employee.count--;
+					employee.$count.innerHTML = employee.count;
+				}
 			}
 		}
 	}
@@ -527,14 +548,16 @@ var real_tick = function() {
 
 // Make sure the correct amount of ticks fire in case of lag or something
 var tick = function() {
-	now_date = new Date();
-	now_ms = now_date.getTime();
+	if ( !paused ) {
+		//now_date = new Date();
+		//now_ms = now_date.getTime();
 
-	//ticks = Math.floor( (now_ms - start_ms) / (1000 / ticks_per_second) );
+		//ticks = Math.floor( (now_ms - start_ms) / (1000 / ticks_per_second) );
 
-	//for ( i = 1; i <= ticks; i++ ) {
-		real_tick();
-	//}
+		//for ( i = 1; i <= ticks; i++ ) {
+			real_tick();
+		//}
+	}
 
 	setTimeout(tick, 1000 / ticks_per_second);
 };
@@ -582,7 +605,7 @@ for ( var prop in game_items ) {
 		var item = game_items[prop];
 		var $element = document.createElement('button');
 		$element.className = item.id;
-		$element.title = item.name;
+		$element.title = item.name + ' - ' + item.description;
 		$items.appendChild($element);
 		$element._item = item;
 	}
@@ -656,7 +679,7 @@ var remove = function($element) {
 
 	$element._settings.item_id = null;
 
-	$element.className = $element.className.replace(casino_item_reg, '')
+	$element.className = $element.className.replace(casino_item_reg, '');
 };
 
 $casino.onmousedown = function(e) {
@@ -705,53 +728,73 @@ $casino.oncontextmenu = function(e) {
 	return false;
 };
 
-$janitor_add.onclick = function() {
-	if ( janitors < janitors_max ) {
-		janitors++;
+$pause.onclick = function() {
+	if ( paused ) {
+		$main.className = $main.className.replace(paused_reg, '');
+		paused = false;
+	} else {
+		$main.className += paused_class;
+		paused = true;
 	}
-
-	update_info();
 };
 
-$janitor_remove.onclick = function() {
-	if ( janitors ) {
-		janitors--;
-	}
+// Employees
+for ( i = 0; i < staff.length; i++ ) {
+	(function() {
+		var employee = staff[i];
 
-	update_info();
-};
+		employee.$count = document.getElementById(employee.plural);
+		employee.$max = document.getElementById(employee.plural + '_max');
 
-$engineer_add.onclick = function() {
-	if ( engineers < engineers_max ) {
-		engineers++;
-	}
+		employee.count = 0;
+		employee.max = 0;
+		employee.quit_reasons = 0;
 
-	update_info();
-};
+		// Add employee
+		document.getElementById(employee.singular + '_add').onclick = function() {
+			if ( employee.count < employee.max ) {
+				employee.count++;
+				update_info();
+			}
+		};
 
-$engineer_remove.onclick = function() {
-	if ( engineers ) {
-		engineers--;
-	}
+		// Fire employee
+		document.getElementById(employee.singular + '_remove').onclick = function() {
+			if ( employee.count ) {
+				employee.count--;
+				update_info();
+			}
+		};
+	})();
+}
 
-	update_info();
-};
+// Ads
+for ( i = 0; i < ads.length; i++ ) {
+	(function() {
+		var ad = ads[i];
 
-$waitress_add.onclick = function() {
-	if ( waitresses < waitresses_max ) {
-		waitresses++;
-	}
+		ad.$count = document.getElementById(ad.name + '_ads');
+		ad.$title_element = document.getElementById(ad.name + '_add');
+		ad.titleTpl = ad.$title_element.title;
+		ad.$title_element.title = ad.titleTpl.replace('{cost}', ad.cost).replace('{prestige}', ad.prestige);
 
-	update_info();
-};
+		ad.count = 0;
 
-$waitress_remove.onclick = function() {
-	if ( waitresses ) {
-		waitresses--;
-	}
+		// Add employee
+		document.getElementById(ad.name + '_add').onclick = function() {
+			ad.count++;
+			update_info();
+		};
 
-	update_info();
-};
+		// Fire employee
+		document.getElementById(ad.name + '_remove').onclick = function() {
+			if ( ad.count ) {
+				ad.count--;
+				update_info();
+			}
+		};
+	})();
+}
 
 // Start game
 start_date = new Date();
@@ -759,6 +802,8 @@ start_ms = start_date.getTime();
 
 $money.innerHTML = money;
 $vault.innerHTML = vault;
+
+$main.className += paused_class;
 
 setTimeout(tick, 1000 / ticks_per_second);
 
